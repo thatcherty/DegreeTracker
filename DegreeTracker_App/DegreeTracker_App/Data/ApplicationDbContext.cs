f2f214f3be6ddce2569a2ad964719c2e63fe9e17
Thatcher <ryeking19@gmail.com>
2026-05-02 12:24:18 -0800

    Added DegreeTracker App

---
+++ DegreeTracker_App/DegreeTracker_App/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DegreeTracker_App.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
